05efa700ada2fe7d84a33ceba39bb7046d592d9b
bariryan <bariryan@gmail.com>
2022-06-27 13:55:00 +0000

    added wiki image to the main page

--- app.js
+++ app.js
@@ -1,29 +1,51 @@
-// set inital value to zero
-let count = 0;
-// select value and buttons
-const value = document.querySelector("#value");
-const btns = document.querySelectorAll(".btn");
+const url =
+  'https://en.wikipedia.org/w/api.php?action=query&list=search&srlimit=20&format=json&origin=*&srsearch=';
 
-btns.forEach(function (btn) {
-  btn.addEventListener("click", function (e) {
-    const styles = e.currentTarget.classList;
-    if (styles.contains("decrease")) {
-      count--;
-    } else if (styles.contains("increase")) {
-      count++;
-    } else {
-      count = 0;
-    }
+const formDOM = document.querySelector('.form');
+const inputDOM = document.querySelector('.form-input');
+const resultsDOM = document.querySelector('.results');
 
-    if (count > 0) {
-      value.style.color = "green";
-    }
-    if (count < 0) {
-      value.style.color = "red";
-    }
-    if (count === 0) {
-      value.style.color = "#222";
-    }
-    value.textContent = count;
-  });
+formDOM.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const value = inputDOM.value;
+  if (!value) {
+    resultsDOM.innerHTML =
+      '<div class="error"> please enter valid search term</div>';
+    return;
+  }
+  fetchPages(value);
 });
+
+const fetchPages = async (searchValue) => {
+  resultsDOM.innerHTML = '<div class="loading"></div>';
+  try {
+    const response = await fetch(`${url}${searchValue}`);
+    const data = await response.json();
+    const results = data.query.search;
+    if (results.length < 1) {
+      resultsDOM.innerHTML =
+        '<div class="error">no matching results. Please try again</div>';
+      return;
+    }
+    renderResults(results);
+  } catch (error) {
+    resultsDOM.innerHTML = '<div class="error"> there was an error...</div>';
+  }
+};
+
+const renderResults = (list) => {
+  const cardsList = list
+    .map((item) => {
+      const { title, snippet, pageid } = item;
+      return `<a href=http://en.wikipedia.org/?curid=${pageid} target="_blank">
+            <h4>${title}</h4>
+            <p>
+              ${snippet}
+            </p>
+          </a>`;
+    })
+    .join('');
+  resultsDOM.innerHTML = `<div class="articles">
+          ${cardsList}
+        </div>`;
+};
